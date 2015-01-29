cb0838d78793934819dec94fc438d73c39a12193
Chris Davies <ferociousturtle@gmail.com>
2015-01-29 12:02:02 -0500

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,5 +1,16 @@
 // User constructs a new user object. For more details, see
 // https://github.com/tiy-durham-fe-2015/curriculum/tree/master/assignments/user_mgmt
 function User(spec) {
-  // TODO: implement the user class, and get user unit tests passing
+  var self = {
+    firstName: spec.firstName,
+    lastName: spec.lastName,
+    email: spec.email,
+
+    equal: function (otherUser) {
+      return self.email === otherUser.email;
+    }
+  };
+
+  return self;
 }
+
