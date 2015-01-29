d881d51a915f9c449ca17060fbe583d8707e6f42
Chris Davies <chris.davies@theironyard.com>
2015-01-28 19:32:43 -0500

    Modifying tests to no longer have userstore as an option

--- js/user-store.js
+++
@@ -1,8 +0,0 @@
-/**
- * UserStore - constructs a new user store object see the assignment instructions for
- * implementation details
- */
-function UserStore() {
-  // TODO: implement the user store class (which should extend the
-  // ObjectStore class)
-}
--- test/user-test.js
+++ test/user-test.js
@@ -6,44 +6,10 @@ qunitUtils.throwsException('spec is required', User);
 
 qunitUtils.requireSpecStrings(['firstName', 'lastName', 'email'], User, makeUser)
 
-QUnit.test('user constructor can take roles', function (assert) {
-  var user = User({
-    firstName: 'Chris',
-    lastName: 'Davies',
-    email: 'foo@bar.com',
-    roles: ['admin', 'user']
-  });
-
-  assert.ok(user.isInRole('user'));
-  assert.ok(user.isInRole('admin'));
-});
-
 QUnit.test('users with the same email should be equal', function (assert) {
   var user1 = makeUser(),
     user2 = makeUser('Joe', 'Shmo', 'js@example.com');
 
   assert.ok(!user1.equal(user2));
   assert.ok(user1.equal(makeUser()));
-});
-
-QUnit.test('add role puts user in role', function(assert) {
-  var user = makeUser(),
-    role = 'admin';
-
-  assert.ok(!user.isInRole(role));
-  user.addRole(role);
-  assert.ok(user.isInRole(role));
-});
-
-QUnit.test('duplicate adds behave like a single add', function (assert) {
-  var user = makeUser(),
-    role = 'student';
-
-  user.addRole(role);
-  user.addRole(role);
-
-  assert.ok(user.isInRole(role));
-
-  user.removeRole(role);
-  assert.ok(!user.isInRole(role));
 });
